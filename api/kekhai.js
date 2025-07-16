5a8edb1b30375194f5f9857731f3575ebf7c7185
admin.hatinh <admin.hatinh@vss.local>
2025-07-16 17:55:39 +0700

    commit

--- api/kekhai.js
+++ api/kekhai.js
@@ -14,6 +14,7 @@ const {
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
+const { log } = require("console");
 
 let checkDB = process.env.SQL_DATABASE;
 let thumucbienlai = "";
